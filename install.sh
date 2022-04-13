8ca1366c9bae794d6a598fe67573071ecc63da3b
nikitamishagin <nikitamishagin@gmail.com>
2022-04-13 14:23:56 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 docker build -t mysite .
 docker run --detach \
-> --name mysite \
-> --env "VIRTUAL_HOST=labstand.myfamcloud.ml" \
-> --env "VIRTUAL_PORT=80" \
-> --env "LETSENCRYPT_HOST=labstand.myfamcloud.ml" \
-> --env "LETSENCRYPT_EMAIL=nikitamishagin@gmail.com" \
-> --network=homecloud_proxy-tier \
-> mysite:latest
+--name mysite \
+--env "VIRTUAL_HOST=labstand.myfamcloud.ml" \
+--env "VIRTUAL_PORT=80" \
+--env "LETSENCRYPT_HOST=labstand.myfamcloud.ml" \
+--env "LETSENCRYPT_EMAIL=nikitamishagin@gmail.com" \
+--network=homecloud_proxy-tier \
+mysite:latest
